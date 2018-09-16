6bae337bbaee571b6bdd4645ad76ac3314db945f
Mariusz Guść <mariuszgusc@gmail.com>
2018-09-16 15:23:21 +0200

    Improving Move for Rafal

--- src/test/java/MoveTest.java
+++ src/test/java/MoveTest.java
@@ -1,4 +1,3 @@
-import org.junit.Before;
 import org.junit.Test;
 import sda.games.chess.Move;
 
@@ -9,7 +8,7 @@ public class MoveTest {
     @Test
     public void inputIsALowerCaseLetters() {
         Move move = new Move("a1>b2");
-        assertEquals("fromX = 0, fromY = 0, toX = 1, toY = 1", move);
+        assertEquals(1 , move.getFromX());
     }
 
 }
