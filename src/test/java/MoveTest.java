fc033d0fb2ae545eb1604a3300e77227fa2885a5
Mariusz Guść <mariuszgusc@gmail.com>
2018-09-18 16:13:08 +0200

    More tests

--- src/test/java/MoveTest.java
+++ src/test/java/MoveTest.java
@@ -6,9 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MoveTest {
 
     @Test
-    public void inputIsALowerCaseLetters() {
+    public void inputIsALowerCaseLetter() {
         Move move = new Move("a1>b2");
         assertEquals(1 , move.getFromX());
     }
+
+    @Test
+    public void inputIsAUpperCaseLetter() {
+        Move move = new Move("A1>B2");
+        assertEquals(1 , move.getFromX());
+    }
+
+    @Test
+    public void onSecondPlaceIsALetter() {
+        Move move = new Move("A1>B2");
+        assertEquals(1 , move.getFromY());
+    }
+
+//    @Test
+//    public void inputStringIsShorterThan5CharsLength() {
+//        Move move = new Move("A1>");
+//        assertEquals(wyjatek);
+//    }
+//
+//    @Test
+//    public void inputStringIsLongerThan5CharsLength() {
+//        Move move = new Move("A1>B2A1>B2");
+//        assertEquals(wyjatek);
+//    }
+
+
 
 }
